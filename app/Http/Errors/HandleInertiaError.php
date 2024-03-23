6f46b7b77b976846b1c64d37b15011d435744431
Craig Riley <68274157+craigrileyuk@users.noreply.github.com>
2024-03-23 11:53:09 +0000

    Fixes for error handler

--- app/Http/Errors/HandleInertiaError.php
+++ app/Http/Errors/HandleInertiaError.php
@@ -7,13 +7,14 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Http\RedirectResponse;
 
 class HandleInertiaError
 {
     public function __invoke(Exceptions $exceptions)
     {
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (/* !app()->environment(['local', 'testing']) &&  */in_array($response->status(), [500, 503, 404, 403])) {
+        $exceptions->respond(function (Response|RedirectResponse $response, Throwable $exception, Request $request) {
+            if (!app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
                 return Inertia::render('Error', ['status' => $response->status()])
                     ->toResponse($request)
                     ->setStatusCode($response->status());
