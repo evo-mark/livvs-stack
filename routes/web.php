cfeef8b0d375625d5f1881bc1fb7b46103fd303f
Craig Riley <68274157+craigrileyuk@users.noreply.github.com>
2024-03-24 09:20:21 +0000

    Fix use statements in routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
